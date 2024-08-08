01dbfe97bd4a25d52e6978df14de68ea47bf9d15
Aanish <aanishrizmy28@gmail.com>
2024-08-08 23:16:01 +0530

    code

--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import { con } from '../utils/db.js'; // Use named import
+import bcrypt, { hash } from 'bcrypt'
+import multer from "multer";
+import path from "path";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -19,6 +23,67 @@ router.post('/adminlogin', (req, res) => {
         } else {
             return res.json({ loginStatus: false, Error: "Wrong username or password" });
         }
+    });
+});
+
+router.get('/state', (req,res)=>{
+    const sql = "SELECT * FROM state";
+    con.query(sql,(err,result)=>{
+        if(err) return res.json({Status:false, Error:"Query Error"})
+            return res.json({Status:true,Result:result})
+    })
+})
+
+router.post('/add_state' , (req,res)=>{
+    const sql = "INSERT INTO state ('name') VALUES (?)"
+    con.query(sql, [req.body.state],(err, result) =>{
+        if(err) return res.json({Status:false, Error:"Query Error"})
+        return res.json({Status:true})
+    })
+})
+
+//image upload
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'Public/Images')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.fildname + "_" + Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({
+    storage:storage
+})
+
+//end image upload
+
+router.post('/add_student', upload.single('image'), (req, res) => {
+    const sql = `INSERT INTO student (SN, AdmissionNo, Name, Class, Gender, state, image) VALUES (?)`;
+    bcrypt.hash(req.body.AdmissionNo, 10, (err, hash) => {
+        if (err) return res.json({ Status: false, Error: "Query Error0" });
+        const values = [
+            req.body.SN,
+            hash,
+            req.body.Name,
+            req.body.Class,
+            req.body.Gender,
+            req.body.state,
+            
+            
+            req.file.filename
+        ];
+        con.query(sql, [values], (err, result) => {
+            if (err) return res.json({ Status: false, Error: err });
+            return res.json({ Status: true });
+        });
+    });
+});
+
+router.get('/add_student', (req, res) => {
+    const sql = "SELECT * FROM student";
+    con.query(sql, (err, result) => {
+        if (err) return res.json({ Status: false, Error: "Query Error" });
+        return res.json({ Status: true, Result: result });
     });
 });
 
